b07f7df8953eeec37b0fb2f628d4799ab8f49571
Vadim Budko <viaduksun@gmail.com>
2021-06-16 14:00:05 +0300

    manifest

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://store:finalprojectfe242021@cluster0.hgx31.mongodb.net/store_test",
+    "mongodb+srv://store:finalprojectfe242021@cluster0.hgx31.mongodb.net/store_test2",
   secretOrKey: "finalprojectfe242021",
 };
